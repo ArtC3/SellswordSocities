5309d035fd4d04de9e1cc08e59bccbe0ea392946
Arthur Catrambone III <rtctrmbn@gmail.com>
2017-01-24 19:38:40 -0600

    Task Scheduler alpha
    Task scheduler now has necessary functionality. DragandDropTest Scene is
    self contained and reads in from the Character Manager script to
    populate a list of choices. The player can select these choices then
    reselect them, drag and drop tasks, ect.
    Next step will be implementing the correct enter and exit into the
    scheduler

---
+++ Assets/Scripts/CharacterDropdownTestScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterDropdownTestScript : MonoBehaviour {
+    [SerializeField] public static List<CharacterScript> FullCharacterList = new List<CharacterScript>();
+    //For testing saving characters
+    public Dropdown dropDown;
+    public InputField inputField;
+	// Use this for initialization
+	void Start () {
+        PopulateDropDown();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void PopulateDropDown()
+    {
+        dropDown.options.Clear();
+        foreach (var character in FullCharacterList)
+        {
+            dropDown.options.Add(new Dropdown.OptionData(character.name));
+        }
+        dropDown.RefreshShownValue();
+    }
+
+    public void RenameCharacter()
+    {
+        foreach (var character in FullCharacterList)
+        {
+            if (character.ID == (dropDown.value + 1))
+            {
+                character.name = inputField.text;
+            }
+        }
+        PopulateDropDown();
+    }
+}
--- Assets/Scripts/TaskPlannerScripts/TabScript.cs
+++ Assets/Scripts/TaskPlannerScripts/TabScript.cs
@@ -5,4 +5,10 @@ using UnityEngine;
 //attach to Button
 public class TabScript : MonoBehaviour {
     public GameObject associatedPanel;
+    [SerializeField]public CharacterScript character;
+
+    public TabScript(CharacterScript _character)
+    {
+        character = _character;
+    }
 }
